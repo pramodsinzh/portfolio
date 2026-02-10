b24771438f4629fa9b75717df050680b96b4160a
Pramodsnz01 <pramodsnz01@gmail.com>
2026-02-10 16:41:09 +0545

    fix vite base path and assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // ✅ REQUIRED for custom domain
+  base: "/", // ✅ REQUIRED
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -12,3 +12,4 @@ export default defineConfig({
     },
   },
 });
+
