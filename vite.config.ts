c704d618a310b6ebbce380ae48b1c6e21cd79c09
Pramodsnz01 <pramodsnz01@gmail.com>
2026-02-10 16:49:50 +0545

    build to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
 
 export default defineConfig({
-  base: "/", // ✅ REQUIRED
+  base: "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
-
+  build: {
+    outDir: "docs",   // 👈 THIS IS THE FIX
+  },
+})
